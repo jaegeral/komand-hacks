604a053c4dae287ae725b8f6885ebc69f1cf1b6c
Alexander J <deralexxx@users.noreply.github.com>
2018-03-06 13:03:29 +0100

    Create create_string_array.py

---
+++ reusable-elements/create_string_array/create_string_array.py
@@ -0,0 +1,8 @@
+def run(params={}):   
+    
+    list = []
+    list.append("lorem")
+    list.append("ipsum=\"phishing\"")
+    list.append("absolute=\"phishing\"")
+    list.append("foobar=\"Phishing\"")
+    return {'list': list}
